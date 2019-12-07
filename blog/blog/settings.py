499a94be3c1829482f8312d5e9bd6c589642bcbb
qiruihua <qiruihua@live.cn>
2019-12-07 10:35:39 +0800

    '添加静态资源并设置静态资源访问路径'

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -120,9 +120,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+#访问静态资源的URL
 STATIC_URL = '/static/'
-
+# 配置静态文件加载路径
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 #设置redis
 CACHES = {
